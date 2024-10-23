1b632f34137a40edda5b39de4c15ff5c20d71524
Dima <151432491+psibladdd@users.noreply.github.com>
2024-10-23 13:24:27 +0300

    Add files via upload

---
+++ src/components/Product/Product.jsx
@@ -0,0 +1,14 @@
+// src/Product.js
+import React from 'react';
+import './Product.css';
+
+const Product = ({ name, price }) => {
+  return (
+    <div className="product-item">
+      <h2>{name}</h2>
+      <p>Price: ${price}</p>
+    </div>
+  );
+};
+
+export default Product;
---
+++ src/hooks/useTelegram.js
@@ -0,0 +1,18 @@
+const tg = window.Telegram.WebApp;
+
+export function useTelegram() {
+    const onClose = () => {
+        tg.close()
+    }
+    const onToggleButton = () => {
+        if(tg.MainButton.isVisible) {
+            tg.MainButton.show()
+        }else{
+            tg.MainButton.hide()
+        }
+    }
+    return {
+        tg,
+        user: tg.initDataUnsafe?.user,
+    }
+}
