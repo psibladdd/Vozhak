431bbe2ac01c8646c65dce6a77be025a97772dad
psibladdd <dimshipunov@gmail.com>
2024-10-24 10:18:53 +0300

    added products
    Signed-off-by: psibladdd <dimshipunov@gmail.com>

--- src/components/Products list/ProductsList.jsx
+++ src/components/Products list/ProductsList.jsx
@@ -7,6 +7,14 @@ const products = [
     {id: 1, name: 'Product 1', price: 100},
     {id: 2, name: 'Product 2', price: 200},
     {id: 3, name: 'Product 3', price: 300},
+    {id: 3, name: 'Product 3', price: 300},
+    {id: 3, name: 'Product 3', price: 300},
+    {id: 3, name: 'Product 3', price: 300},
+    {id: 3, name: 'Product 3', price: 300},
+    {id: 3, name: 'Product 3', price: 300},
+    {id: 3, name: 'Product 3', price: 300},
+    {id: 3, name: 'Product 3', price: 300},
+    {id: 3, name: 'Product 3', price: 300},
     // Добавьте больше продуктов по необходимости
 ];
 
