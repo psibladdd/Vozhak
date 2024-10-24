70989639bbbfe1ca9dd95b5d39e9915309e0c16b
Dima <151432491+psibladdd@users.noreply.github.com>
2024-10-24 17:13:47 +0300

    Add files via upload

--- src/components/ProductDetail/ProductDetail.jsx
+++ src/components/ProductDetail/ProductDetail.jsx
@@ -2,30 +2,35 @@
 import React from 'react';
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
 import {faArrowLeft} from '@fortawesome/free-solid-svg-icons';
-import './ProductDetail.css'
+import './ProductDetail.css';
 import {useLocation, useNavigate} from 'react-router-dom';
 
 const ProductDetail = () => {
     const location = useLocation();
-
     const navigate = useNavigate();
     const {id, name, price, image, desc} = location.state;
 
     if (!id) {
         return <div>Product not found</div>;
     }
+
     const handleBack = () => {
         navigate(-1); // Возвращает на предыдущую страницу
     };
+
     return (
-        <div>
-            <div className="back_button" onClick={handleBack}>
+        <div className="product-detail-container">
+            <div className="back-button-container" onClick={handleBack}>
                 <FontAwesomeIcon icon={faArrowLeft}/>
             </div>
-            <img className="photo" src={`/photos/${image}`} alt={name}/>
-            <h2>{name}</h2>
-            <p>Price: ${price}</p>
-            <p>{desc}</p>
+            <div className="product-info-container">
+                <img className="photo" src={`/photos/${image}`} alt={name}/>
+                <div className="product-text-container">
+                    <h2>{name}</h2>
+                    <p>Price: ${price}</p>
+                </div>
+                <p className="product-description">{desc}</p>
+            </div>
         </div>
     );
 };
