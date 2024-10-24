dc315879b3f9580e3280625850d20800cb480c67
psibladdd <dimshipunov@gmail.com>
2024-10-24 10:36:47 +0300

    link
    Signed-off-by: psibladdd <dimshipunov@gmail.com>

--- src/components/Product/Product.jsx
+++ src/components/Product/Product.jsx
@@ -4,10 +4,12 @@ import './Product.css';
 
 const Product = ({name, price}) => {
     return (
-        <a className="product-item" href="/">
-            <h2>{name}</h2>
-            <p>Price: ${price}</p>
-        </a>
+        <div className="product-item">
+            <a>
+                <h2>{name}</h2>
+                <p>Price: ${price}</p>
+            </a>
+        </div>
     );
 };
 
