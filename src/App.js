7f0a486cc36f99d609558139832de3c42fdd64d1
psibladdd <dimshipunov@gmail.com>
2024-10-24 13:36:20 +0300

    link
    Signed-off-by: psibladdd <dimshipunov@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,28 +1,17 @@
-import './App.css';
-import Header from "./components/Header/Header";
-import {useEffect} from "react";
-import {useTelegram} from "./hooks/useTelegram";
+// src/App.js
+import React from 'react';
 import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 import ProductsList from './components/Products list/ProductsList';
 import ProductDetail from './components/ProductDetail/ProductDetail';
 
 function App() {
-    const {tg, onToggleButton} = useTelegram();
-
-    useEffect(() => {
-        tg.ready();
-    }, []);
-
     return (
-        <div className="App">
-            <Header/>
-            <Router>
-                <Routes>
-                    <Route path="/" element={<ProductsList/>}/>
-                    <Route path="/product/:id" element={<ProductDetail/>}/>
-                </Routes>
-            </Router>
-        </div>
+        <Router>
+            <Routes>
+                <Route path="/" element={<ProductsList/>}/>
+                <Route path="/product/:id" element={<ProductDetail/>}/>
+            </Routes>
+        </Router>
     );
 }
 
